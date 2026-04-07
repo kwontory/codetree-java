c374b48ad6adcab98685d922beaa204e52087d5b
Jihyeon Kwon <victory.jihyeon@gmail.com>
2026-04-07 22:47:57 +0900

    Title: 2개 이상의 알파벳, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260407/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        // Please write your code here.
+        if(서로다른알파벳몇개(A) > 3) {
+            System.out.print("Yes");
+        } else {
+            System.out.print("No");
+        }
+    }
+
+    public static int 서로다른알파벳몇개(String s) {
+        char[] sArr = s.toUpperCase().toCharArray(); // 나는야 대문자가 좋아~
+        int[] cnts = new int[128];
+        for(char c : sArr) {
+            cnts[c] = 1;
+        }
+        int total = 0;
+        for(int n : cnts) {
+            total += n;
+        }
+
+        return total;
+    }
+}
