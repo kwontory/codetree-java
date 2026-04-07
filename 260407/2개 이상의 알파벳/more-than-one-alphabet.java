3f668ba874ac164150952c2c12e9b011cfcf6a4a
Jihyeon Kwon <victory.jihyeon@gmail.com>
2026-04-07 22:48:57 +0900

    Title: 2개 이상의 알파벳, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260407/2개 이상의 알파벳/more-than-one-alphabet.java
+++ 260407/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -4,7 +4,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String A = sc.next();
         // Please write your code here.
-        if(서로다른알파벳몇개(A) > 3) {
+        if(서로다른알파벳몇개(A) >= 2) {
             System.out.print("Yes");
         } else {
             System.out.print("No");
