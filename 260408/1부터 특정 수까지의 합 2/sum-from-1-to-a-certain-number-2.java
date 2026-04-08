5f92ffb36a0ffdbf82a2b7e36113fba4b3115d62
Jihyeon Kwon <victory.jihyeon@gmail.com>
2026-04-08 15:34:02 +0900

    Title: 1부터 특정 수까지의 합 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260408/1부터 특정 수까지의 합 2/sum-from-1-to-a-certain-number-2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(f(n));
+    }
+
+    public static int f(int n) {
+        if(n == 0) return 0;
+        return f(n-1) + n;
+    }
+}
