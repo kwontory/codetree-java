85f6cbb2ac2225970d3cd29dc77f17a547691e14
Jihyeon Kwon <victory.jihyeon@gmail.com>
2026-04-08 15:01:54 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260408/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        f(n);
+    }
+
+    public static void f(int n) {
+        if(n < 1) return;
+        for(int i=0;i<n;i++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+        f(n-1);
+        for(int i=0;i<n;i++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+}
