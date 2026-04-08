8daa8b7a544b6793c727687a813fc9a67b8ee9d8
Jihyeon Kwon <victory.jihyeon@gmail.com>
2026-04-08 15:56:01 +0900

    Title: 각 자리 숫자의 제곱, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260408/각 자리 숫자의 제곱/square-of-each-digit.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(f(n));
+    }
+
+    public static int f(int n) {
+        if(n < 10) return n*n;
+        return f(n/10) + (n%10)*(n%10);
+    }
+}
