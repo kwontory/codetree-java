d3fe1b338d211e6d1ee9f0a344872a4ebc79421c
Jihyeon Kwon <victory.jihyeon@gmail.com>
2026-04-08 16:46:31 +0900

    Title: Factorial, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260408/Factorial/factorial.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(f(n));
+    }
+    public static int f(int n) {
+        if(n==0) return 1;
+        if(n==1) return 1;
+        return n*f(n-1);
+    }
+}
