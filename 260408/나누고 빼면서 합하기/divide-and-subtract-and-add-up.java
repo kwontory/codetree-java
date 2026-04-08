ad6363896c11a2927a2f9d0ebbb807489b93f487
Jihyeon Kwon <victory.jihyeon@gmail.com>
2026-04-08 12:44:57 +0900

    Title: 나누고 빼면서 합하기, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260408/나누고 빼면서 합하기/divide-and-subtract-and-add-up.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static int n, m;
+    public static int[] arr;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[n];
+        for (int i = 1; i < n; i++)
+            arr[i] = sc.nextInt();
+        int sum = 0;
+        while(m > 0) {
+            sum += arr[m];
+            manipulate();
+        }
+        System.out.print(sum);
+    }
+
+    public static void manipulate() {
+        if(m%2==1) {
+            m -= 1;
+        } else {
+            m /= 2;
+        }
+    }
+}
