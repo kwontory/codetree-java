9defd20ed34585b674de241a6b28200819b5f54f
Jihyeon Kwon <victory.jihyeon@gmail.com>
2026-04-08 13:12:51 +0900

    Title: 특정 구간의 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260408/특정 구간의 합/sum-of-specific-intervals.java
+++ 260408/특정 구간의 합/sum-of-specific-intervals.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
     public static int MAX_VALUE = 100;
-    public static int[] a = new int[MAX_VALUE];
+    public static int[] a = new int[MAX_VALUE + 1];
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
