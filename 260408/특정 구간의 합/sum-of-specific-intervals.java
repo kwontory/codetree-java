a6cb7b7cce1e03802f8bb8734a2efd065ac03f13
Jihyeon Kwon <victory.jihyeon@gmail.com>
2026-04-08 13:12:03 +0900

    Title: 특정 구간의 합, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260408/특정 구간의 합/sum-of-specific-intervals.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static int MAX_VALUE = 100;
+    public static int[] a = new int[MAX_VALUE];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        for (int i = 1; i <=n; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < m; i++) {
+            int a1 = sc.nextInt();
+            int a2 = sc.nextInt();
+            // Please write your code here.
+            System.out.println(sum(a1,a2));
+        }
+    }
+    public static int sum(int start, int end) {
+        int sum = 0;
+        for(int i=start;i<=end;i++) {
+            sum+= a[i];
+        }
+        return sum;
+    }
+}
